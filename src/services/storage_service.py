6a33936db02cec0fe30b59b9af923fa44fe36d64
Evgenii <gnezdilovzena40@gmail.com>
2025-12-17 12:32:44 +0300

    update: Полностью изменен файл с сохранением состояний

--- src/services/storage_service.py
+++ src/services/storage_service.py
@@ -1,15 +1,28 @@
-# сервис хранения
-
 import json
+import os
 
-class StorageService:
-    def save(self, filename, data):
-        with open(filename, "w", encoding="utf-8") as f:
-            json.dump(data, f, ensure_ascii=False, indent=2)
 
-    def load(self, filename):
+class StateStorage:
+    def __init__(self, filename="/Users/evgenii/Documents/Лабы/Лабы Проектирование ПО/KPO/src/data/device_state.json"):
+        self.filename = filename
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
+
+    def save(self, data: dict):
+        temp_file = self.filename + ".tmp"
+        with open(temp_file, "w", encoding="utf-8") as f:
+            json.dump(data, f, indent=4, ensure_ascii=False)
+        os.replace(temp_file, self.filename)
+
+    def load(self) -> dict:
+        if not os.path.exists(self.filename):
+            return {}
+
         try:
-            with open(filename, "r", encoding="utf-8") as f:
-                return json.load(f)
-        except FileNotFoundError:
-            return None
+            with open(self.filename, "r", encoding="utf-8") as f:
+                content = f.read().strip()
+                if not content:
+                    return {}
+                return json.loads(content)
+        except (json.JSONDecodeError, OSError):
+            # если файл битый или пустой — начинаем с чистого состояния
+            return {}
